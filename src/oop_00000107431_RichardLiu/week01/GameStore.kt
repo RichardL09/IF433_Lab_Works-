213b3866d4ccba0fa591a6ebd852817cc7cd120f
Richard Liu <liusenhauer.com>
2026-02-05 00:19:40 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107431_RichardLiu/week01/GameStore.kt
+++ src/oop_00000107431_RichardLiu/week01/GameStore.kt
@@ -4,4 +4,14 @@ fun main() {
     // Data game
     val gameTitle = "Pokemon Scarlet and Violet Double Pack version"
     val price = 1500000
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+// Menghitung besar diskon berdasarkan harga game
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price * 20 / 100
+    } else {
+        price * 10 / 100
+    }
