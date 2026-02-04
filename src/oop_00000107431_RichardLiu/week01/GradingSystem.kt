0426d4265442387a2fe7b8369f541d8747918163
Richard Liu <liusenhauer.com>
2026-02-04 20:56:20 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000107431_RichardLiu/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000107431_RichardLiu.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
