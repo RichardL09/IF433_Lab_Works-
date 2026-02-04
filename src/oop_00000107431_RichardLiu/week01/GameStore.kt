82aeac817a20c9c9d7ef7dab1a9ad6e14f615677
Richard Liu <liusenhauer.com>
2026-02-05 00:26:32 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000107431_RichardLiu/week01/GameStore.kt
+++ src/oop_00000107431_RichardLiu/week01/GameStore.kt
@@ -1,17 +1,21 @@
 package oop_00000107431_RichardLiu.week01
 
 fun main() {
-    // Data game
     val gameTitle = "Pokemon Scarlet and Violet Double Pack version"
     val price = 1500000
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+    printReceipt(
+        title = gameTitle,
+        price = price,
+        finalPrice = finalPrice
+    )
 }
 
-// Menghitung besar diskon berdasarkan harga game
-fun calculateDiscount(price: Int): Int =
-    if (price > 500000) {
-        price * 20 / 100
-    } else {
-        price * 10 / 100
-    }
+fun calculateDiscount(price: Int): Int = if (price > 500000) price * 20 / 100 else price * 10 / 100
+
+fun printReceipt(title: String, price: Int, finalPrice: Int) {
+    println("Game Title  : $title")
+    println("Price: $price")
+    println("Final Price : Rp $finalPrice")
+}
