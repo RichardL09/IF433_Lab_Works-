2accfd230f63a56e3da78b27cb5e7d9cc206839a
Richard Liu <liusenhauer.com>
2026-02-04 21:30:06 +0700

    week01: implement null safety check

--- src/oop_00000107431_RichardLiu/week01/GradingSystem.kt
+++ src/oop_00000107431_RichardLiu/week01/GradingSystem.kt
@@ -19,6 +19,14 @@ fun main() {
 
     // Panggil DI DALAM main ()
     println("Status: ${calculateStatus(score)}")
+
+    // Tambahkan di akhir main ()
+    val studentId: String? = null
+
+    // Jika null, gunakan nilai default 0
+    val idLenght = studentId?.length ?: 0
+
+    println("Panjang ID: $idLenght")
 }
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
