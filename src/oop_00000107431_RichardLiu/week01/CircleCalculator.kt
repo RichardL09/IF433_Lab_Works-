29f879a47ea4d087ccbe8dd86da112c1df31a9c8
Richard Liu <liusenhauer.com>
2026-02-04 22:37:10 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000107431_RichardLiu/week01/CircleCalculator.kt
+++ src/oop_00000107431_RichardLiu/week01/CircleCalculator.kt
@@ -1,18 +1,14 @@
 package oop_00000107431_RichardLiu.week01
 
-fun main(args: Array<String>) {
-    // Variable definition (Ubah var menjadi val, dan hapus tipe data ekspilit)
+fun main() {
+    // Ubah var menjadi val, dan hapus tipe data ekspilit
     val radius = 7.0
     val pi = 3.14
-
-    // Calculation (Ubah var menjadi val dan hapus tipe data ekspilit)
     val area = pi * radius * radius
 
-    // Output String Template (Ubah println menjadi String Template) + Panggil fungsi checksize didalam println
+    // Ubah println menjadi String Template + Panggil fungsi checksize didalam println
     println("Radius: $radius, Area: $area, ${checkSize(area)}")
 }
-
-// Mengubah signature
 fun checkSize(area: Double) =
     if (area > 100){
         "This is a Big Circle"
