8017f89e1e46d68c582b0ff57877342ec197d640
Richard Liu <liusenhauer.com>
2026-02-04 21:12:10 +0700

    week01: refactor to use val and string templates

--- src/oop_00000107431_RichardLiu/week01/GradingSystem.kt
+++ src/oop_00000107431_RichardLiu/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_00000107431_RichardLiu.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    // REFACTOR: Gunakan val dan hapus tipe data ekspilit
+    val name = "John Thor"
+    val score = 80
 
     // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
