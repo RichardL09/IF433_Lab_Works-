ff190e094a615d6829bc5bf75b30061f66d5837b
Richard Liu <liusenhauer.com>
2026-02-11 15:43:48 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000107431_RichardLiu/week02/Student.kt
+++ src/oop_00000107431_RichardLiu/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000107431_RichardLiu.week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ){
     init {
         // Validasi Sederhana: Cek panjang NIM
