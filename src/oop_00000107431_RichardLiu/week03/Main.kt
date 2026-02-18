d31cc1bb985465d7118c8bf1d7e1296e45e631a3
Richard Liu <liusenhauer.com>
2026-02-18 10:57:10 +0700

    week03: cleanup code

--- src/oop_00000107431_RichardLiu/week03/Main.kt
+++ src/oop_00000107431_RichardLiu/week03/Main.kt
@@ -4,7 +4,7 @@ fun main(){
     val e = Employee("Budi")
 
     // 1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print Error
+    // e.salary = -1000 // Harusnya print Error
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
