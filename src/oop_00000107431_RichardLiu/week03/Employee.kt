bd36bf84b05f8afd71f817c64a9a062983849bbf
Richard Liu <liusenhauer.com>
2026-02-18 10:47:46 +0700

    week03: add private property

--- src/oop_00000107431_RichardLiu/week03/Employee.kt
+++ src/oop_00000107431_RichardLiu/week03/Employee.kt
@@ -3,11 +3,26 @@ package oop_00000107431_RichardLiu.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            if (value < 0) {
+            if(value < 0){
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0")
                 field = 0 // Gunakan field, bukan this.salary
             } else {
                 field = value // Gunakan field untuk assign nilai asli
             }
         }
+
+    // Hanya bisa diakses di file/ class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformance(){
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
+    // kecuali kita buat function khusus untuk print.
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
+
