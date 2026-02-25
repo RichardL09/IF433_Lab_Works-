88f00927f6037356f0f17ec0e4faad0e430519b0
Richard Liu <liusenhauer.com>
2026-02-25 16:04:46 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000107431_RichardLiu/week04/Main.kt
+++ src/oop_00000107431_RichardLiu/week04/Main.kt
@@ -6,9 +6,15 @@ fun main() {
     generalVehicle.honk()
     generalVehicle.accelerate()
 
-    println("\n--- Testing Car ---")n
+    println("\n--- Testing Car ---")
     val myCar = Car("Toyota", 4)
     myCar.openTrunk() // Memanggil method milik sendiri
     myCar.honk()      // Memanggil method yang sudah di-override
     myCar.accelerate()// Memanggil gabungan method Parent dan Child
+
+    println("\n--- Testing Electric Car")
+    val electricCar = ElectricCar("Tesla", 4, 85)
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
 }
