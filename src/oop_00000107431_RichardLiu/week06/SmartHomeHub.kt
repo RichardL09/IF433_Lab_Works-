53a862a58d38622834c6de6b431c632c17dd6228
Richard Liu <liusenhauer.com>
2026-03-11 11:23:24 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000107431_RichardLiu/week06/SmartHomeHub.kt
@@ -0,0 +1,22 @@
+package oop_00000107431_RichardLiu.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} ditambahkan ke Smart Home Hub")
+    }
+
+    fun turnOffAllSwitches() {
+
+        for (device in devices) {
+
+            if (device is Switchable) {
+                device.turnOff()
+            }
+
+        }
+    }
+}
