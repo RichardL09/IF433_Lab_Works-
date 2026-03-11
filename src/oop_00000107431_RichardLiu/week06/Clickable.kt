d274e5f652d6d9fe04c1534f51db89394543f02b
Richard Liu <liusenhauer.com>
2026-03-11 10:20:09 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000107431_RichardLiu/week06/Clickable.kt
+++ src/oop_00000107431_RichardLiu/week06/Clickable.kt
@@ -1,9 +1,6 @@
 package oop_00000107431_RichardLiu.week06
 
 interface Clickable {
-    // ERROR : Property in an interface cannot have a backing field
-    val name: String = "Tombol Rahasia"
-
-    // Function without body (Implicity Abstract)
+    val name: String // Abstract property, tidak ada nilai default
     fun click()
 }
