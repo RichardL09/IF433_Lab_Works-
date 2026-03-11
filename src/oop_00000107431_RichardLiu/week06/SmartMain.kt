4fcfd2ce832e3bd0671c6207694353ef242bd9af
Richard Liu <liusenhauer.com>
2026-03-11 11:47:32 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000107431_RichardLiu/week06/SmartMain.kt
+++ src/oop_00000107431_RichardLiu/week06/SmartMain.kt
@@ -1,9 +1,17 @@
 package oop_00000107431_RichardLiu.week06
 
 fun main() {
-
     val lamp = SmartLamp("1", "Ruang Tamu")
     val speaker = SmartSpeaker("2", "Google Nest Dapur")
     val cctv = SmartCCTV("3", "Ezviz Garasi")
 
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+
+    hub.turnOffAllSwitches()
 }
