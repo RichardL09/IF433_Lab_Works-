4905c2573ba5b4e2b1a9e7be7477372fb1cbe845
Richard Liu <liusenhauer.com>
2026-03-11 11:41:24 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000107431_RichardLiu/week06/SmartMain.kt
@@ -0,0 +1,9 @@
+package oop_00000107431_RichardLiu.week06
+
+fun main() {
+
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+}
