5d1cd55c1bcf25bd69e67f23d877be7601e25f77
Richard Liu <liusenhauer.com>
2026-04-15 10:35:14 +0700

    week08: implement java interop using !!

--- src/oop_00000107431_RichardLiu/week08/LegacyJavaAPI.java
+++ src/oop_00000107431_RichardLiu/week08/LegacyJavaAPI.java
@@ -3,6 +3,6 @@ package oop_00000107431_RichardLiu.week08;
 public class LegacyJavaAPI {
     // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable
     public static String fetchServerStatus() {
-        return "SERVER_ONLINE_200_OK"
+        return "SERVER_ONLINE_200_OK";
     }
 }
