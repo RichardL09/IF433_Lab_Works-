1f8e59d4fb40f01b704d54ff6d302f84ea988999
Richard Liu <liusenhauer.com>
2026-04-22 10:23:56 +0700

    week09: chain filter, sortedBy, and map on object collection

--- src/oop_00000107431_RichardLiu/week09/ObjectChaining.kt
+++ src/oop_00000107431_RichardLiu/week09/ObjectChaining.kt
@@ -8,6 +8,14 @@ fun main() {
         Student("Budi", 3.9),
         Student("Citra", 2.8),
         Student("Dewi", 3.7),
-
     )
+
+    println("=== HONOR STUDENTS PIPELINE ===")
+
+    // Kita butuh NAMA mahasiswa yang GPA-nya >= 3.5, dan diurutkan abjad
+    val honorNames = students
+        .filter { it.gpa >= 3.5 }      // 1. Buang yang GPA < 3.5
+        .sortedBy { it.name }          // 2. Urutkan berdasarkan nama
+        .map { it.name.uppercase () }  // 3. Ambil namanya saja dan jadikan kapital
+    honorNames.forEach { println("Honor Roll: $it")}
 }
