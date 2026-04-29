fe358e3207b92040f7212c707cb7889245aee3a1
Richard Liu <liusenhauer.com>
2026-04-29 08:47:32 +0700

    week10: create generic Box class

---
+++ src/oop_00000107431_RichardLiu/week10/GenericBox.kt
@@ -0,0 +1,4 @@
+package oop_00000107431_RichardLiu.week10
+
+// Kelas generic menggunakan <T> sebagai placeholder tipe data
+class Box<T>(var value: T)
---
+++ src/oop_00000107431_RichardLiu/week10lec/M10ClassA.kt
@@ -0,0 +1,37 @@
+package oop_00000107431_RichardLiu.week10lec
+
+// Non-Generic Programming
+class meja(val hasil:Any)
+
+// Generic Programming
+class kursi<T>(val hasil: T)
+
+// Generic Programming
+class Televisi<T, V>(val merk:T, val kodeBarcode: V)
+
+// Generic - Function
+fun <T> Alamat(kodePos:T):T {
+    return kodePos
+}
+
+
+fun main() {
+    // Implementasi Non-Generic
+    println("======= implementasi Non-Generic =======")
+    val mj = meja(100)
+    val hargaMeja = mj.hasil as Int
+    println(hargaMeja + 50)
+
+    // Implementasi Generic
+    println("\n======= implementasi Generic =======")
+    val chair = kursi(300)
+    println(chair.hasil + 50)
+
+    println("\n======= implementasi Multi Param =======")
+    val tv = Televisi("Samsung", 884777)
+    println("Merknya " + tv.merk)
+    println("Barcode " + tv.kodeBarcode + 77777)
+
+    println("\n======= Generic Function =======")
+    println("Kode pos kamu" + Alamat(154133))
+}
