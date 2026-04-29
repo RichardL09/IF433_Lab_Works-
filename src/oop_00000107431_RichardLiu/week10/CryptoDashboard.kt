f9d2b8638c1058b3d5f4ae80893c87c523c210a1
Richard Liu <liusenhauer.com>
2026-04-29 09:25:40 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000107431_RichardLiu/week10/CryptoDashboard.kt
+++ src/oop_00000107431_RichardLiu/week10/CryptoDashboard.kt
@@ -7,4 +7,9 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.25))
     coinRepo.add(Coin("ETH", 12.5))
     coinRepo.add(Coin("USDT", 5000.0))
+
+    val response = ApiResponse(
+        "200 OK",
+        coinRepo.getAll()
+    )
 }
