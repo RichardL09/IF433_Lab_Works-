c95cfc823ada8700cb0bb5249d7f6acbab0484df
Richard Liu <liusenhauer.com>
2026-05-06 09:58:34 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000107431_RichardLiu/week11/SmartHomeDashboard.kt
+++ src/oop_00000107431_RichardLiu/week11/SmartHomeDashboard.kt
@@ -2,10 +2,21 @@ package oop_00000107431_RichardLiu.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
     SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
     }.also {
         homeDevices.add(it)
     }
+
+    SmartDevice("Ezviz Outdoor", "Camera")
+        .apply {
+            isOnline = true
+            powerLoad = 5
+        }
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
 }
