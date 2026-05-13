1da849ec76722653233a8037314cd6fdb7b81752
Richard Liu <liusenhauer.com>
2026-05-13 09:29:30 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_00000107431_RichardLiu/week12/SmartFeeder.kt
+++ src/oop_00000107431_RichardLiu/week12/SmartFeeder.kt
@@ -30,5 +30,7 @@ fun main() {
         println("Error: ${e.message}")
     } catch (e: Exception) {
         println("Error umum: ${e.message}")
+    } finally {
+        println("Siklus pengecekan dispenser pagi selesai.")
     }
 }
