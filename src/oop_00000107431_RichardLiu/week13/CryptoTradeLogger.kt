171d593d6d0894c4cc0ac1fe6475287178f251ca
Richard Liu <liusenhauer.com>
2026-05-20 13:30:18 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_00000107431_RichardLiu/week13/CryptoTradeLogger.kt
+++ src/oop_00000107431_RichardLiu/week13/CryptoTradeLogger.kt
@@ -1,5 +1,6 @@
 package oop_00000107431_RichardLiu.week13
 import java.io.File
+import java.io.FileNotFoundException
 
 data class TradeRecord(
     val id: Int,
@@ -35,5 +36,15 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach {
             writer.println(it.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path)
+            .readLines()
+            .mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        emptyList()
     }
 }
